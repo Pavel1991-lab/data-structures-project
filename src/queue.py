60fcab4ada5c875f958adab42486902016e9ceb9
Павел <sotnikov.pavel.91@mail.ru>
2023-05-19 16:21:56 +0300

    home-34

--- src/queue.py
+++ src/queue.py
@@ -1,11 +1,12 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
+        :param next_node: ссылка на следующий узел
         """
         self.data = data
         self.next_node = next_node
@@ -18,38 +19,26 @@ class Queue:
         """Конструктор класса Queue"""
         self.head = None
         self.tail = None
+        self.size = 0
 
     def enqueue(self, data):
-        """
-        Метод для добавления элемента в очередь
-
-        :param data: данные, которые будут добавлены в очередь
-        """
-        new_node = Node(data, None)
-        if self.tail is None:
+        """Добавление элемента в конец очереди"""
+        new_node = Node(data)
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
+        elif self.tail is None:
+            self.tail = new_node
+            self.head.next_node = self.tail
         else:
             self.tail.next_node = new_node
             self.tail = new_node
-
-    def dequeue(self):
-        """
-        Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
-
-        :return: данные удаленного элемента
-        """
-        if self.head is None:
-            raise Exception("Очередь пуста")
-        else:
-            data = self.head.data
-            self.head = self.head.next_node
-            if self.head is None:
-                self.tail = None
-            return data
+        self.size += 1
 
     def __str__(self):
         """Магический метод для строкового представления объекта"""
+        if self.head is None:
+            return ""
         result = []
         node = self.head
         while node is not None:
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,41 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.append(item)
+
+    def dequeue(self):
+        return self.items.pop(0)
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def size(self):
+        return len(self.items)
+
+    def __str__(self):
+        return str(self.items)
+
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        self.queue = Queue()
+
+    def test_enqueue(self):
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.assertEqual(str(self.queue), "[1, 2]")
+
+    def test_str_empty_queue(self):
+        self.assertEqual(str(self.queue), "[]")
+
+    def test_str_non_empty_queue(self):
+        self.queue.enqueue(1)
+        self.queue.enqueue(2)
+        self.assertEqual(str(self.queue), "[1, 2]")
+
+if __name__ == "__main__":
+    unittest.main()
